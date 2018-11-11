9a142dab91bc941d782268d9658532ae6fd1df12
Matt Little <zone117x@gmail.com>
2018-11-11 14:52:02 -0800

    Unit tests for moneroCore local functions and MyMonero API usage.

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,4 @@
-import * as mocha from 'mocha';
-import { describe, it, before, beforeEach, after, afterEach } from 'mocha';
-import * as assert from 'assert';
+import { assert } from 'chai';
 import { MoneroWallet } from './moneroWallet';
 
 
@@ -13,41 +11,60 @@ describe('Monero Wallet - local functions', () => {
     it('Should create account keys from private key', () => {
         let privateKey = '9be111fbf5e01bb8e1bd741b6cce7dd73b7d8a65e0de44d0b98c212e2accc00e';
         let keys = MoneroWallet.createAddressKeysFromPrivateKey(privateKey);
+        assert.equal(keys.mnemonic, "muppet sensible sack idiom rarest inquest ruined western atom diode pause leech academy grunt sash session hickory deity inbound catch gang yawning older pivot gang");
         assert.equal(keys.privateKeys.spend, privateKey);
+        assert.equal(keys.publicAddress, "43Pzz5GFHzG4VSvoR1zievZmQ3ABZppFagWsQkdpLwV2JH5YCY3bmyvFjXxcd1XAnYS6P4bWzx1YvATW7gZNUg7t3paPcE6");
     });
 
     it('Should create account keys from mnemonic', () => {
         let mnemonic = 'veteran gnome asylum exhale noted knapsack camp vats wonders biggest ablaze awful ablaze';
         let keys = MoneroWallet.createAddressKeysFromMnemonic(mnemonic);
         assert.equal(keys.mnemonic, mnemonic);
+        assert.equal(keys.privateKeys.spend, "9be111fbf5e01bb8e1bd741b6cce7dd73b7d8a65e0de44d0b98c212e2accc00e");
+        assert.equal(keys.publicAddress, "43Pzz5GFHzG4VSvoR1zievZmQ3ABZppFagWsQkdpLwV2JMmu2LLU5GgHmSbVqc7dBMAYi49BHXD3cTLWX3D4LX8k4q1AXQf");
     });
 
 });
 
+describe('Monero Wallet - MyMonero API', () => {
 
-async function tests() {
-    await MoneroWallet.initialize();
+    let moneroWallet : MoneroWallet;
 
-    let keys1 = MoneroWallet.createAddressKeysFromPrivateKey("9be111fbf5e01bb8e1bd741b6cce7dd73b7d8a65e0de44d0b98c212e2accc00e");
-    let keys2 = MoneroWallet.createAddressKeysFromMnemonic("veteran gnome asylum exhale noted knapsack camp vats wonders biggest ablaze awful ablaze");
-    
-    //let keys3 = MoneroWallet.createAddressKeysFromPrivateKey("323cb7ba59b5857ca767500bbf0a9b137f3d445a217826d89b8f61bd5bbe950e");
-    //let keys4 = MoneroWallet.createAddressKeysFromMnemonic("input betting five balding update licks hive february dogs peaches ongoing digit five");
-    let moneroWallet = new MoneroWallet(keys1);
-    //let registrationResult = await moneroWallet.registerNewAccount();
-    //console.log(registrationResult);
+    before(async () => {
+        await MoneroWallet.initialize();
+        let keys = MoneroWallet.createAddressKeysFromMnemonic("input betting five balding update licks hive february dogs peaches ongoing digit five");
+        moneroWallet = new MoneroWallet(keys);
+    });
 
-    let txs = await moneroWallet.getTransactions();
-    console.log(txs);
-    let walletInfo = await moneroWallet.getBalanceInfo();
-    console.log(walletInfo);
+    it('Should allow account login / register', async () => {
+        let registrationResult = await moneroWallet.registerNewAccount();
+        console.log(registrationResult);
+        assert.equal(registrationResult.alreadyRegistered, true);
+    });
 
-    //let toAddress = "43Pzz5GFHzG4VSvoR1zievZmQ3ABZppFagWsQkdpLwV2JMmu2LLU5GgHmSbVqc7dBMAYi49BHXD3cTLWX3D4LX8k4q1AXQf";
-    //let sendResult = await moneroWallet.sendFunds(toAddress, new BigNumber("0.00001"));
-    //console.log(sendResult);
-}
+    it('Should get wallet balance info', async () => {
+        let walletInfo = await moneroWallet.getBalanceInfo();
+        console.log(walletInfo);
+        assert.isAbove(parseFloat(walletInfo.totalReceived), 0);
+        assert.isAbove(parseFloat(walletInfo.totalSent), 0);
+    });
 
+    it('Should get transaction info', async() => {
+        let txs = await moneroWallet.getTransactions();
+        console.log(txs);
+        assert.isAbove(txs.accountScannedBlockHeight, 0);
+        assert.isAbove(txs.accountScannedTxHeight, 0);
+        assert.isAbove(txs.accountScanStartHeight, 0);
+        assert.isAbove(txs.blockchainHeight, 0);
+        assert.isAbove(txs.transactionHeight, 0);
+    });
 
-//tests().catch(err => {
-//    console.log(err);
-//});
+    it('Should send funds', async () => {
+        let toAddress = "43Pzz5GFHzG4VSvoR1zievZmQ3ABZppFagWsQkdpLwV2JMmu2LLU5GgHmSbVqc7dBMAYi49BHXD3cTLWX3D4LX8k4q1AXQf";
+        let sendResult = await moneroWallet.sendFunds(toAddress, "0.00001");
+        console.log(sendResult);
+        assert.equal(sendResult.toAddress, toAddress);
+        assert.equal(sendResult.sentAmount, 0);
+    });
+
+});
